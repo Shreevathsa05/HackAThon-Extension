7754f24f39384807fa8ff0d6bf9054b177ab371b
Shreevathsa05 <144523306+Shreevathsa05@users.noreply.github.com>
2025-10-08 22:46:18 +0530

    Update Uploader.jsx

--- frontend/src/components/Uploader.jsx
+++ frontend/src/components/Uploader.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { useParams } from "react-router";
 
-const API_BASE = "https://teachers-toolkit-ai-backend-latest.onrender.com";
+const API_BASE = "https://hackathon-extension.onrender.com";
 const colors = {
   teal: "#006D77",
   mint: "#83C5BE",
@@ -63,3 +63,4 @@ const btnStyle = (bg) => ({
   background: bg, color: "#fff", border: "none",
   padding: "8px 12px", borderRadius: 8, cursor: "pointer",
 });
+
